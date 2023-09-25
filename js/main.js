beec0e7892cb154d160786c0b649a4742e7cfb1f
Diogorussini <129057390+Diogorussini@users.noreply.github.com>
2023-09-25 08:51:23 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Criar variáveis no Javascript
+// var letra = 'O'; var é uma versão em desuso
+// const letra = 'D';
+//console.log(letra);
+
+//Iniciando uma variável que representa um jogador
+let letra = 'X' ;
+
+function joga(cel) {
+    let casaClicada = document.getElementById(cel).innerHTML;
+    // innerHTML - injeta tags HTML na página
+    if(casaClicada == "X"){
+        
+    }
+}
